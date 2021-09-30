087794a7dc7d1b5b3c11634d7be7b1f34336db0f
albert <yicheng@cxist.com>
2021-09-30 18:21:00 +0800

    new visitor

--- MongoLinq.Tests/QueryTest.cs
+++ MongoLinq.Tests/QueryTest.cs
@@ -160,6 +160,9 @@ namespace MongoLinq.Tests
                 from s2 in StudentSet
                 where s2.Name != "a"
                 select s2;
+            var q2 = SchoolSet.SelectMany(s1 => StudentSet, (s1, s2) => new { s1, s2 })
+                .Where(@t => @t.s2.Name != "a")
+                .Select(@t => @t.s2);
             var list = q.ToList();
             foreach (var item in list)
             {
@@ -168,7 +171,24 @@ namespace MongoLinq.Tests
         }
 
        
-
+        [Fact]
+        public void SelectMany2()
+        {
+            var q = from s1 in SchoolSet
+                where s1.Name == "kk"
+                from s2 in StudentSet
+                where s2.Name != "a"
+                select s2;
+            var q2 = SchoolSet.Where(s1 => s1.Name == "kk")
+                .SelectMany(s1 => StudentSet, (s1, s2) => new { s1, s2 })
+                .Where(@t => @t.s2.Name != "a")
+                .Select(@t => @t.s2);
+            var list = q.ToList();
+            foreach (var item in list)
+            {
+                Logger.WriteLine(JsonConvert.SerializeObject(item));
+            }
+        }
        
        
       
---
+++ MongoLinqs/Pipelines/MyVisitor.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace MongoLinqs.Pipelines
+{
+    public class MyVisitor
+    {
+        private readonly StringBuilder _builder;
+        private List<string> _indexes;
+
+        public MyVisitor(Expression expression)
+        {
+            _builder = new StringBuilder();
+            _indexes = new List<string>();
+            MainCollection = null;
+            VisitQuery(expression);
+        }
+
+        public string MainCollection { get; }
+
+        public string Pipeline => _builder.ToString();
+
+        private void VisitQuery(Expression expression)
+        {
+            switch (expression)
+            {
+                case MethodCallExpression methodCall:
+                    VisitLinqMethod(methodCall);
+                    break;
+                case ConstantExpression constant:
+                    VisitDbSet(constant);
+                    break;
+                default:
+                    throw new NotSupportedException();
+            }
+        }
+
+        #region Linq Method
+
+        private void VisitLinqMethod(MethodCallExpression expression)
+        {
+            switch (expression.Method.Name)
+            {
+                case "Where":
+                    break;
+                case "Select":
+                    break;
+                default:
+                    throw new NotSupportedException();
+            }
+        }
+        
+        private void VisitWhere(MethodCallExpression node)
+        {
+            var source = node.Arguments[0];
+            VisitQuery(source);
+
+            var builder = new StringBuilder();
+
+            builder.Append("{");
+            builder.Append("\"$match\":");
+            var lambda = (node.Arguments[1] as UnaryExpression)!.Operand as LambdaExpression;
+
+            var result = new LambdaBodyBuilder(lambda!.Body, lambda.Parameters.Count > 1).Build(true, true);
+            builder.Append(result);
+            builder.Append("}");
+            _builder.Append(builder.ToString());
+        }
+
+        #endregion
+
+        public void VisitDbSet(ConstantExpression expression)
+        {
+            
+        }
+    }
+}
