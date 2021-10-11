052a38c447a962d753007a37f8c279f0f0c3b1a1
albert <yicheng@cxist.com>
2021-10-11 18:52:46 +0800

    go

---
+++ MongoLinq.Tests/MyVisitorTest.cs
@@ -0,0 +1,147 @@
+﻿using System.Linq;
+using MongoLinq.Tests.Common;
+using MongoLinq.Tests.Entities;
+using MongoLinqs.Pipelines;
+using Newtonsoft.Json;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace MongoLinq.Tests
+{
+    public class MyVisitorTest : TestBase
+    {
+       
+
+        public MyVisitorTest(ITestOutputHelper testOutputHelper) : base(testOutputHelper)
+        {
+        }
+
+        
+        [Fact]
+        public void Where()
+        {
+            var q = from s in StudentSet
+                where s.Id == 2  && s.Name == "bbb"
+                select new {Id2 = s.Id, s.Name, s.Enabled, s.SchoolId};
+
+            
+            var q1 = StudentSet
+                .Where(s => s.Id == 2 && s.Name.Contains("bb") && s.Name == "bbb")
+                .Select(s => new { Id2 = s.Id, s.Name, s.Enabled, s.SchoolId });
+
+
+            var foo = new MyVisitor(q.Expression);
+
+        }
+
+        [Fact]
+        public void WhereEqual()
+        {
+            var q = from s in StudentSet
+                where s.Id == 2 
+                select new {Id2 = s.Id, s.Name, s.Enabled, s.SchoolId};
+
+       
+        }
+        
+        [Fact]
+        public void WhereNotEqual()
+        {
+            var q = from s in StudentSet
+                where  s.Name != "bbb"
+                select new {Id2 = s.Id, s.Name, s.Enabled, s.SchoolId};
+
+         
+        }
+        
+        [Fact]
+        public void WhereStringContains()
+        {
+            var q = from s in StudentSet
+                where  s.Name.Contains("bb")
+                select new {Id2 = s.Id, s.Name, s.Enabled, s.SchoolId};
+
+         
+        }
+
+        [Fact]
+        public void SelectNew()
+        {
+            var q = from s in StudentSet
+                // where  s.Id == 2 && s.Name.Contains("bb") && s.Name == "bbb" 
+                select new {Id2 = s.Id, s.Name, s.Enabled, s.SchoolId};
+
+        }
+
+        [Fact]
+        public void SelectNewInit()
+        {
+            var q = from s in StudentSet
+                select new Student() {Id = s.Id, Name = "dd", Enabled = s.Enabled,};
+
+         
+        }
+
+        [Fact]
+        public void SelectNewNull()
+        {
+            var q = from s in StudentSet
+                select new Student() {Id = s.Id, Name = null, Enabled = s.Enabled,};
+
+         
+        }
+
+      
+
+
+        [Fact]
+        public void SelectDirect()
+        {
+            var q = from s in SchoolSet
+                select s;
+        
+
+          
+        }
+        
+        [Fact]
+        public void SelectId()
+        {
+            var q = from s in SchoolSet
+                select s;
+          
+        }
+
+        [Fact]
+        public void SelectMany()
+        {
+            var q = from s1 in SchoolSet
+                from s2 in StudentSet
+                where s2.Name != "a"
+                select s2;
+            var q2 = SchoolSet.SelectMany(s1 => StudentSet, (s1, s2) => new { s1, s2 })
+                .Where(@t => @t.s2.Name != "a")
+                .Select(@t => @t.s2);
+           
+        }
+
+       
+        [Fact]
+        public void SelectMany2()
+        {
+            var q = from s1 in SchoolSet
+                where s1.Name == "kk"
+                from s2 in StudentSet
+                where s2.Name != "a"
+                select s2;
+            var q2 = SchoolSet.Where(s1 => s1.Name == "kk")
+                .SelectMany(s1 => StudentSet, (s1, s2) => new { s1, s2 })
+                .Where(@t => @t.s2.Name != "a")
+                .Select(@t => @t.s2);
+           
+        }
+       
+       
+      
+    }
+}
--- MongoLinqs/Pipelines/MyVisitor.cs
+++ MongoLinqs/Pipelines/MyVisitor.cs
@@ -1,26 +1,66 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq.Expressions;
 using System.Text;
+using System.Threading.Tasks.Dataflow;
+using Newtonsoft.Json;
 
 namespace MongoLinqs.Pipelines
 {
     public class MyVisitor
     {
         private readonly StringBuilder _builder;
-        private List<string> _indexes;
+        private readonly List<int> _indexes;
+        private string _mainCollection;
 
         public MyVisitor(Expression expression)
         {
             _builder = new StringBuilder();
-            _indexes = new List<string>();
-            MainCollection = null;
+            _indexes = new List<int>();
+            Push();
+            _builder.Append("[");
             VisitQuery(expression);
+            _builder.Append("]");
+            Pop();
         }
 
-        public string MainCollection { get; }
+        public string MainCollection => _mainCollection;
 
-        public string Pipeline => _builder.ToString();
+        public string Pipeline => Format(_builder.ToString());
+
+        private void Push()
+        {
+            _indexes.Add(0);
+        }
+
+        private void Pop()
+        {
+            _indexes.RemoveAt(_indexes.Count - 1);
+        }
+
+        private void Next()
+        {
+            _indexes[^1] += 1;
+        }
+
+
+        private static string Format(string json)
+        {
+            return json;
+            using var stringReader = new StringReader(json);
+            using var stringWriter = new StringWriter();
+            var jsonReader = new JsonTextReader(stringReader);
+            var jsonWriter = new JsonTextWriter(stringWriter) { Formatting = Formatting.Indented };
+            jsonWriter.WriteToken(jsonReader);
+            return stringWriter.ToString();
+        }
+
+        private static LambdaExpression GetLambda(Expression expression)
+        {
+            var unary = (UnaryExpression)expression;
+            return (LambdaExpression)unary.Operand;
+        }
 
         private void VisitQuery(Expression expression)
         {
@@ -44,36 +84,107 @@ namespace MongoLinqs.Pipelines
             switch (expression.Method.Name)
             {
                 case "Where":
+                    VisitWhere(expression);
                     break;
                 case "Select":
+                    VisitSelect(expression);
                     break;
                 default:
                     throw new NotSupportedException();
             }
         }
-        
+
         private void VisitWhere(MethodCallExpression node)
         {
-            var source = node.Arguments[0];
-            VisitQuery(source);
+            VisitQuery(node.Arguments[0]);
 
-            var builder = new StringBuilder();
+            if (_indexes[^1] != 0)
+            {
+                _builder.Append(",");
+            }
 
-            builder.Append("{");
-            builder.Append("\"$match\":");
-            var lambda = (node.Arguments[1] as UnaryExpression)!.Operand as LambdaExpression;
+            _builder.Append("{$match:{");
 
-            var result = new LambdaBodyBuilder(lambda!.Body, lambda.Parameters.Count > 1).Build(true, true);
-            builder.Append(result);
-            builder.Append("}");
-            _builder.Append(builder.ToString());
+            VisitLambda(GetLambda(node.Arguments[1]));
+
+            _builder.Append("}}");
+            Next();
+        }
+
+        private void VisitSelect(MethodCallExpression node)
+        {
+            VisitQuery(node.Arguments[0]);
+            if (_indexes[^1] != 0)
+            {
+                _builder.Append(",");
+            }
+
+            _builder.Append("{$project:{");
+            _builder.Append("}}");
+            Next();
         }
 
         #endregion
 
-        public void VisitDbSet(ConstantExpression expression)
+        #region Db Set
+
+        private void VisitDbSet(ConstantExpression expression)
         {
-            
+            if (_indexes.Count == 1)
+            {
+                _mainCollection = "abc";
+            }
         }
+
+        #endregion
+
+        #region Lambda
+
+        private void VisitLambda(LambdaExpression expression)
+        {
+            var @params = expression.Parameters;
+            VisitExpression(expression.Body);
+        }
+
+        private void VisitExpression(Expression expression)
+        {
+            switch (expression.NodeType)
+            {
+                case ExpressionType.AndAlso:
+                    VisitAndAlso((BinaryExpression)expression);
+                    break;
+                case ExpressionType.Equal:
+                    VisitEqual((BinaryExpression)expression);
+                    break;
+                default:
+                    _builder.Append("\"*\"");
+                    break;
+            }
+        }
+
+
+        private void VisitAndAlso(BinaryExpression expression)
+        {
+            _builder.Append("{$expr:{");
+            _builder.Append("$and:[");
+            VisitExpression(expression.Left);
+            _builder.Append(",");
+            VisitExpression(expression.Right);
+            _builder.Append("]");
+            _builder.Append("}}");
+        }
+        
+        private void VisitEqual(BinaryExpression expression)
+        {
+            _builder.Append("{$expr:{");
+            _builder.Append("$eq:[");
+            VisitExpression(expression.Left);
+            _builder.Append(",");
+            VisitExpression(expression.Right);
+            _builder.Append("]");
+            _builder.Append("}}");
+        }
+
+        #endregion
     }
 }
